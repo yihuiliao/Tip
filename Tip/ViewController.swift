eeaa2e0bd066ad85ea2e3410d7a25f7e02e5dd32
Yihui Liao <liao.yihui18@gmail.com>
2020-07-09 17:12:57 -0400

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Yihui Liao on 7/9/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
