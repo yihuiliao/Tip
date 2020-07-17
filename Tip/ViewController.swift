c99185e6111808954d61c8fae63cfc05357dadc5
yihuiliao <liao.yihui18@gmail.com>
2020-07-17 18:37:06 -0400

    added feature to split the bill among 1-8 people

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+        
     
     @IBOutlet weak var tipLabel: UILabel!
     
@@ -19,27 +20,44 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var splitControl: UISegmentedControl!
+    
+    @IBOutlet weak var splitLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        billField.becomeFirstResponder()
+        
+    }
+    
+    //If the user taps off the text field, the keyboard will close
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
+    
+  
     @IBAction func calculateTip(_ sender: Any) {
-        
+
         let bill = Double(billField.text!) ?? 0
-        
-        
+
+
         let tipPercentages = [0.15, 0.18, 0.20]
+        let splitPeople = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
         let total = bill + tip
         
+        let split = (bill + tip)/splitPeople[splitControl.selectedSegmentIndex]
+
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
-        
+        splitLabel.text = String(format: "$%.2f", split)
+
     }
 }
 
